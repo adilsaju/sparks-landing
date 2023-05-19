ea93434f28adc03cdd3fe0a54b7976842d7693c5
Adil Saju <adilsaju@gmail.com>
2023-05-19 00:09:38 -0700

    format

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-document.querySelector('.scroll-button').addEventListener('click', function() {
-    const cardContainer = document.querySelector('.card-container');
-    cardContainer.scrollBy({ left: 200, behavior: 'smooth' });
+document.querySelector('.scroll-button').addEventListener('click', function () {
+  const cardContainer = document.querySelector('.card-container');
+  cardContainer.scrollBy({
+    left: 200,
+    behavior: 'smooth'
   });
-  
-  const a = document.querySelectorAll('.card-button')
+});
+
+const a = document.querySelectorAll('.card-button')
 
 for (let i = 0; i < a.length; i++) {
-    a[i].addEventListener('mouseover', function (event) {
-        event.target.innerHTML = event.target.getAttribute('data-text');
-    });
-    a[i].addEventListener('mouseout', function (event) {
-        event.target.innerHTML = "Register";
-    });
+  a[i].addEventListener('mouseover', function (event) {
+    event.target.innerHTML = event.target.getAttribute('data-text');
+  });
+  a[i].addEventListener('mouseout', function (event) {
+    event.target.innerHTML = "Register";
+  });
 }
 
-window.onload = function() {
-        let container = document.querySelector('.card-container2');
-        let scrollAmount = 0;
-      
-        let slideTimer = setInterval(function(){
-            container.scrollLeft += 1;
-            scrollAmount++;
-        }, 25);
-      
-        container.addEventListener('scroll', function() {
-          if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
-            container.scrollLeft = 0;
-          }
-        });
-  }
+window.onload = function () {
+  let container = document.querySelector('.card-container2');
+  let scrollAmount = 0;
+
+  let slideTimer = setInterval(function () {
+    container.scrollLeft += 1;
+    scrollAmount++;
+  }, 25);
+
+  container.addEventListener('scroll', function () {
+    if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+      container.scrollLeft = 0;
+    }
+  });
+}
