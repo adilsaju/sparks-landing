79e019cf109b8a2896ac20b3a85d2ec66dce964e
Adil Saju <adilsaju@gmail.com>
2023-05-18 20:40:27 -0700

    hover done

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ document.querySelector('.scroll-button').addEventListener('click', function() {
     cardContainer.scrollBy({ left: 200, behavior: 'smooth' });
   });
   
+  const a = document.querySelectorAll('.card-button')
+
+for (let i = 0; i < a.length; i++) {
+    a[i].addEventListener('mouseover', function (event) {
+        event.target.innerHTML = event.target.getAttribute('data-text');
+    });
+    a[i].addEventListener('mouseout', function (event) {
+        event.target.innerHTML = "Register";
+    });
+}
